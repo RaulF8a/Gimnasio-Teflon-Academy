201a6b359096e1200c9cd6917ef40124ca817f51
Raul Ochoa <raulochoa039@gmail.com>
2022-11-11 20:17:41 -0600

    Crear header y footer

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+import ejs from 'ejs';
 
 const app = express ();
+const __dirname = path.resolve ();
 
+app.use (bodyParser.urlencoded ({extended: true}));
+app.use (express.static ("public"));
+app.set ("view engine", "ejs");
 
+// 
+
+app.get ("/", (req, res) => {
+    res.render ("home")
+});
 
 app.listen (process.env.PORT || 3000, () => {
     console.log ("Server running on port 3000");
