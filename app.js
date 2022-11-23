3c5dd080536676154a7719ee230fdfb88d5d4403
JavierCortesArmas <javierarmas499@gmail.com>
2022-11-23 14:53:37 -0600

    Editar Empleado

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express ();
 const __dirname = path.resolve ();
 
 // Conexion a la base de datos.
-let conexion = mysql2.createConnection ({
+/*let conexion = mysql2.createConnection ({
     host: "localhost",
     user: "root",
     password: process.env.DB_PASSWORD,
@@ -21,7 +21,7 @@ conexion.connect ((err) => {
     if (err) throw err;
     
     console.log ("Conexion exitosa.");
-});
+});*/
 
 // Configuraciones de aplicacion.
 app.use (bodyParser.urlencoded ({extended: true}));
@@ -46,7 +46,7 @@ let atletaBuscado = {
     puesto:"" 
 };
 let carrito = [];
-let sesionIniciada = false;
+let sesionIniciada = true;
 let campoEditar = "";
 let idEditar = "";
 let cuentaBuscada = {};
